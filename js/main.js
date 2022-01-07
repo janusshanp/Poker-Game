c17928e44fe52965dd2749372b1fff5c4ea3a042
janusshan <janusshan.p@gmail.com>
2022-01-07 03:11:59 -0500

    finish and clean js

--- js/main.js
+++ js/main.js
@@ -1,303 +1,323 @@
 /*---------constants---------*/
-let suits = ['H','S','D','C']
-let cardValues = ['02','03','04','05','06','07','08','09','10','11','12','13','14']
-
+let suits = ["H", "S", "D", "C"];
+let cardValues = [
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+];
 
 /*---------app's state (variables)---------*/
 
-//change so that it can have an input
-var firstPlay = false 
-var secondDeal = false 
-let win = 0
+//to determine if this is the first user clicks Deal
+let firstPlay = false;
+//to determine if this is the second time a user clocks Deal
+let secondDeal = false;
+//win multiplier
+let win = 0;
 
-let displayCards = {
-    card1: { card:"", selected: false },
-    card2: { card:"", selected: false },
-    card3: { card:"", selected: false },
-    card4: { card:"", selected: false },
-    card5: { card:"", selected: false }
-}
+//cards to be displayed to user
+let displayCards = [
+  { card: "", selected: false },
+  { card: "", selected: false },
+  { card: "", selected: false },
+  { card: "", selected: false },
+  { card: "", selected: false },
+];
 
-//change this location when you clean up your code 
-let betCredits = document.querySelector('.credits span')
-
+//init function to create deck and take inputs from modal
 let init = {
-    cardDeck: [],
-    totalCredits: 0,
-    currentBet: 0,    
-    betInput: function () {
-      init.totalCredits = prompt("Enter Total Credits:")
-      betCredits.textContent = init.totalCredits
-    },
-    currentBetInput: function () {
-      betDisplay.textContent = "Current Bet: " + init.currentBet
-      init.totalCredits = init.totalCredits - init.currentBet
-      betCredits.textContent = "Total Credits: "+ init.totalCredits      
-    },
-    createDeck: function () {
-        suits.forEach(suit => cardValues.forEach(value => init.cardDeck.push(suit.concat(value))))
-      }
-}
-
-init.betInput();
+  cardDeck: [],
+  name: "",
+  totalCredits: 0,
+  currentBet: 0,
+  createDeck: function () {
+    suits.forEach((suit) =>
+      cardValues.forEach((value) => init.cardDeck.push(suit.concat(value)))
+    );
+  },
+};
 
 /*---------cached element references---------*/
-let cardC = document.querySelectorAll('.card')
-let dealButton = document.querySelector('#Deal')
-let messageC = document.querySelectorAll('.message')
-let winMessage = document.querySelector('.win-display')
-let betNum = document.querySelector('.bet span')
-let betUpButton = document.querySelector('#B-up')
-let betDownButton = document.querySelector('#B-down')
-let cashOut = document.querySelector('#quit')
-
+let cardC = document.querySelectorAll(".card");
+let dealButton = document.querySelector("#Deal");
+let messageC = document.querySelectorAll(".message");
+let winMessage = document.querySelector(".win-display");
+let betNum = document.querySelector(".bet span");
+let betUpButton = document.querySelector("#B-up");
+let betDownButton = document.querySelector("#B-down");
+let cashOut = document.querySelector("#quit");
+let initButton = document.querySelector(".modal-content button");
+let modalDisp = document.querySelector(".init-modal");
+let betCredits = document.querySelector(".credits span");
+let endOfTurnMessage = document.querySelector(".play-again");
 
 /*---------event listener---------*/
-
-function createEventListeners () {
-  for (let i = 0; i < cardC.length; i++){
-    cardC[i].addEventListener("click", function(e) {
-      let cardID = 'card'+[i+1]
-      if(secondDeal){
-        return
-      } 
-      if (displayCards[cardID].selected){
-        displayCards[cardID].selected = false
-        messageC[i].textContent =""
-        cardC[i].classList.remove("outline")
+function createEventListeners() {
+  for (let i = 0; i < cardC.length; i++) {
+    cardC[i].addEventListener("click", function (e) {
+      if (secondDeal) return;
+      if (displayCards[i].selected) {
+        displayCards[i].selected = false;
+        messageC[i].textContent = "";
+        cardC[i].classList.remove("outline");
       } else {
-        displayCards[cardID].selected = true
-        messageC[i].textContent ="Hold"
-        cardC[i].classList.add("outline")
+        displayCards[i].selected = true;
+        messageC[i].textContent = "Hold";
+        cardC[i].classList.add("outline");
       }
-      console.log("clicked")
-    })
+    });
   }
 }
 
-dealButton.addEventListener("click", mainGame) 
+initButton.addEventListener("click", initFunction);
 
-betUpButton.addEventListener("click", betFunction)
-betDownButton.addEventListener("click",betFunction)
+dealButton.addEventListener("click", mainGame);
 
-cashOut.addEventListener("click",quitFunction)
+betUpButton.addEventListener("click", betFunction);
+betDownButton.addEventListener("click", betFunction);
 
+cashOut.addEventListener("click", quitFunction);
 
+/*---------functions---------*/
+//function to take values from the modal and store them
+function initFunction() {
+  let nameInp = document.querySelector("#name-input").value;
+  let totalInp = document.querySelector("#total-input").value;
+  init.name = nameInp;
+  init.totalCredits = totalInp;
+  betCredits.textContent = init.totalCredits;
+  modalDisp.style.display = "none";
+}
 
-
-/*---------functions---------*/ 
+//quit Function which stops the game and cashes out player
 function quitFunction() {
-  dealButton.disabled = true
-  betDownButton.disabled = true
-  betUpButton.disabled = true  
-  winMessage.textContent = `Congratulations you have won ${init.totalCredits+init.currentBet}
-Credits!`}
+  dealButton.disabled = true;
+  betDownButton.disabled = true;
+  betUpButton.disabled = true;
+  winMessage.textContent = `Congratulations you have won ${
+    init.totalCredits + init.currentBet
+  } Credits!`;
+  endOfTurnMessage.textContent = `Thanks for playing, come back with more Money!`;
+}
 
+//to dynamically change credits and bet function
 function betFunction(e) {
-  if(e.target.id == 'B-up'){
-    init.currentBet++
-    init.totalCredits--
-  }else if(e.target.id == 'B-down'){
-    init.currentBet--
-    init.totalCredits++
+  if (e.target.id == "B-up" && init.totalCredits !== 0) {
+    init.currentBet++;
+    init.totalCredits--;
+  } else if (e.target.id == "B-down" && init.currentBet !== 0) {
+    init.currentBet--;
+    init.totalCredits++;
   }
-  betNum.textContent = init.currentBet
-  betCredits.textContent = init.totalCredits
+  betNum.textContent = init.currentBet;
+  betCredits.textContent = init.totalCredits;
 }
 
-function mainGame () {
-  var time = 0
-  console.log("i clicked!")
-  if (!firstPlay){
-    createEventListeners ()
-    firstPlay = true
+//main function game will run in
+function mainGame() {
+  if (!firstPlay) {
+    createEventListeners();
+    firstPlay = true;
   }
-  if (init.cardDeck.length == 0){
-    console.log("1dfsd")
-    init.createDeck ()
-    var interval = setInterval(function() {
-    if(time<=4){
-      chooseCards(time)
+  if (init.cardDeck.length == 0) {
+    init.createDeck();
+    chooseCards();
+    cardFlip();
+    betUpButton.disabled = true;
+    betDownButton.disabled = true;
+  } else if (betUpButton.disabled && betDownButton.disabled) {
+    secondDeal = true;
+    chooseCards();
+    cardFlip();
+    betUpButton.disabled = false;
+    betDownButton.disabled = false;
+  } else if (secondDeal) {
+    resetCards();
+    mainGame();
+  }
+}
+//function to choose cards and store the numbers in displaycards 
+function chooseCards() {
+  for(let i=0; i < displayCards.length; i++){
+    if (displayCards[i].selected === false) {
+      if (displayCards[i].card !== "") {
+        cardC[i].classList.replace(displayCards[i].card,"back-red");
+      }
+      let rInt = Math.floor(Math.random() * init.cardDeck.length);
+      displayCards[i].card = init.cardDeck[rInt];
+      init.cardDeck.splice(rInt, 1);
+    }
+  }
+}
+
+function cardFlip() {
+  let cardsToChangeidx =[]
+  for (let i=0; i < displayCards.length; i++){
+    messageC[i].textContent =""
+    if (displayCards[i].selected === false){
+      cardsToChangeidx.push(i)
+    }
+  }
+  let time = 0;
+  let interval = setInterval(function () {
+    if (time < cardsToChangeidx.length) {
+      cardC[cardsToChangeidx[time]].classList.remove("back-red"); 
+      cardC[cardsToChangeidx[time]].classList.add(displayCards[cardsToChangeidx[time]].card) 
       time++;
+    } else {
+      checkCards();
+      clearInterval(interval);
+      if(secondDeal){
+        totalBet();
+      }
     }
-    else {
-      checkCards()
-      clearInterval(interval)
-    }
-    },500)
-    betUpButton.disabled = true
-    betDownButton.disabled =true 
-  } else if (betUpButton.disabled && betDownButton.disabled){
-      console.log("Hello")
-      secondDeal = true
-      var interval = setInterval(function() {
-        if(time<=4) {
-          chooseCards(time)
-          time++
-        }
-        else {
-          checkCards()
-          clearInterval(interval)
-          totalBet()
-        }
-      },500) 
-      betUpButton.disabled = false
-      betDownButton.disabled = false
-  } else if (secondDeal){
-      console.log("ibanma")
-      resetCards ()
-      mainGame () 
-}
+  }, 200);
 }
 
-function resetCards () {
-  let i = 0 
+//function to resetCards
+function resetCards() {
+  let i = 0;
   for (cards in displayCards) {
-    cardC[i].classList.remove(displayCards[cards].card)
-    displayCards[cards].card = ""
-    displayCards[cards].selected = false
-    messageC[i].textContent = ""
-    i++
+    cardC[i].classList.replace(displayCards[cards].card,"back-red");
+    displayCards[cards].card = "";
+    displayCards[cards].selected = false;
+    messageC[i].textContent = "";
+    i++;
   }
-  init.cardDeck = []
-  win = 0 
-  secondDeal = false 
+  init.cardDeck = [];
+  win = 0;
+  secondDeal = false;
 }
 
-function totalBet () {
-  init.totalCredits = init.totalCredits + (win * init.currentBet)
-  betCredits.textContent = init.totalCredits
-  betNum.textContent = 0 
-  init.currentBet = 0 
-}
-
-//need to figure out how to stop duplicate cards in dealing  
-function chooseCards (time) {
-  let cardID = 'card' + (time+1)
-  console.log(cardID)
-  cardC[time].classList.remove("back-red")
-  if (displayCards[cardID].selected === false){
-    if (displayCards[cardID].card !== ''){
-    cardC[time].classList.remove(displayCards[cardID].card)
-    }
-    let rInt = Math.floor(Math.random()*init.cardDeck.length)
-    displayCards[cardID].card = init.cardDeck[rInt]
-    cardC[time].classList.add(init.cardDeck[rInt])
-    init.cardDeck.splice(rInt,1)
+function totalBet() {
+  init.totalCredits = init.totalCredits + win * init.currentBet;
+  betCredits.textContent = init.totalCredits;
+  betNum.textContent = 0;
+  init.currentBet = 0;
+  for (let i = 0; i < 5; i++) {
+    cardC[i].classList.remove("outline");
   }
+  endOfTurnMessage.textContent = `${init.name} keep going you're going to be rich!!`;
 }
 
-function checkCards () {
-    // winning conditions 
-    //has to be a bettter way to do this????????!?!?!?!?!?!
-    // state variables which we will manipulate in this function 
-    let finalCards =[]
-    let finalCardsValues = []
-    let finalCardsSuits =[]
-    let fiveSuits = false
-    let valuesinOrder = false 
-    let fourPair = false 
-    let threePair = false
-    let twoPair1 = false
-    let twoPair2 = false
-    let jacksorBetter = false
 
-    //for loop to push cards in object to an array to better maninpulate? 
-    //do we even need to do this in a better method?
-    for (cards in displayCards){
-        finalCards.push(displayCards[cards].card) 
-    }
-    //sorting here because if sorted in for loop later it will put 10 infront 
-    //since it converts into string?
-    // there must be a way to do this without sorting so its easier and more dynamic?
-    // try to clean up the code here 
-    //loop to seperate out the suits and values 
-    for (card of finalCards) {
-        finalCardsValues.push(parseInt(card[1]+card[2]))
-        finalCardsSuits.push(card[0])
-    }
-    
-    finalCardsValues.sort((function(a,b){return a-b}))
+// winning condition functions
+function checkCards() {
+  // variables which we will manipulate in this function to check our conditions
+  let finalCards = [];
+  let finalCardsValues = [];
+  let finalCardsSuits = [];
+  let fiveSuits = false;
+  let valuesinOrder = false;
+  let fourPair = false;
+  let threePair = false;
+  let twoPair1 = false;
+  let twoPair2 = false;
+  let jacksorBetter = false;
 
-    //to tally up how many of a suit we have 
-    console.log(finalCardsSuits)
-    console.log(finalCardsValues)
+  //to first take the values from the cards displayed into a new array
+  for (cards in displayCards) {
+    finalCards.push(displayCards[cards].card);
+  }
+  //to split up the suits and the values
+  for (card of finalCards) {
+    finalCardsValues.push(parseInt(card[1] + card[2]));
+    finalCardsSuits.push(card[0]);
+  }
 
-    let suitTally = finalCardsSuits.reduce(function(acc,suit){
-        acc[suit] = acc[suit] ? acc[suit] +1 : 1;
-        return acc;
-    }, {})
-    // to tally up how many of each number we have 
-    let numberTally = finalCardsValues.reduce(function(acc,value){
-        acc[value] = acc[value] ? acc[value] +1 : 1;
-        return acc;
-    }, {})
-    // to see if we have five of the same suits 
-    for (suit in suitTally){
-        if (suitTally[suit]===5){
-          fiveSuits = true 
-          // what about every in a for loop? 
-        } 
+  //sort the array based on value so that we can see if its in order
+  finalCardsValues.sort(function (a, b) {
+    return a - b;
+  });
+
+  //to tally up how many of a suit we have
+
+  let suitTally = finalCardsSuits.reduce(function (acc, suit) {
+    acc[suit] = acc[suit] ? acc[suit] + 1 : 1;
+    return acc;
+  }, {});
+  // to tally up how many of each number we have
+  let numberTally = finalCardsValues.reduce(function (acc, value) {
+    acc[value] = acc[value] ? acc[value] + 1 : 1;
+    return acc;
+  }, {});
+  // to see if we have five of the same suits
+  for (suit in suitTally) {
+    if (suitTally[suit] === 5) {
+      fiveSuits = true;
     }
-    // to see how many of the values are either 4 of a kind, 3 of a kind, two pair or just 1 pair 
-    //also checks if jack or better is true 
-    for (num in numberTally){
-        if (numberTally[num]===4){
-          fourPair = true 
-        }
-         if (numberTally[num]===3){
-          threePair = true
-        }
-        if (numberTally[num]===2 && twoPair1 === false){
-          twoPair1 = true
-          if (num == 11 || num == 12 || num == 13 || num == 14) {
-            jacksorBetter = true
-            }
-        continue
-        }
-        if (numberTally[num]===2){
-          twoPair2 = true
-        }
+  }
+  // to see how many of the values are either 4 of a kind, 3 of a kind, two pair or just 1 pair
+  //also checks if jack or better is true
+  for (num in numberTally) {
+    if (numberTally[num] === 4) {
+      fourPair = true;
     }
-    // to see if cards are in order 
-    for (let i =0; i < 4; i ++){
-      if (finalCardsValues[i+1]-finalCardsValues[i] === 1){
-        valuesinOrder = true 
-      } else {
-        valuesinOrder = false
-        break
+    if (numberTally[num] === 3) {
+      threePair = true;
+    }
+    if (numberTally[num] === 2 && twoPair1 === false) {
+      twoPair1 = true;
+      if (num == 11 || num == 12 || num == 13 || num == 14) {
+        jacksorBetter = true;
       }
+      continue;
     }
+    if (numberTally[num] === 2) {
+      twoPair2 = true;
+    }
+  }
+  // to see if cards are in order, else is there since if beginning numbers are in order it will return true
+  // and then it will return false once it loops and finds a number that isnt false and break
+  for (let i = 0; i < 4; i++) {
+    if (finalCardsValues[i + 1] - finalCardsValues[i] === 1) {
+      valuesinOrder = true;
+    } else {
+      valuesinOrder = false;
+      break;
+    }
+  }
 
-    if (valuesinOrder && fiveSuits && Math.min(...finalCardsValues)===10){
-        winMessage.textContent = "Royal Flush" 
-        win = 250
-      } else if (valuesinOrder && fiveSuits) {
-        winMessage.textContent = "Straight Flush"
-        win = 50 
-      } else if (fourPair){
-        winMessage.textContent = "Four Pair"
-        win = 25
-      } else if (threePair && twoPair1){
-        winMessage.textContent = "Full House"
-        win = 9
-      } else if (fiveSuits){
-        winMessage.textContent = "Flush"
-        win = 6
-      } else if (valuesinOrder){
-        winMessage.textContent = "Straight"
-        win = 4
-      } else if (threePair){
-        winMessage.textContent = "Three Pair"
-        win = 3
-      } else if (twoPair1 && twoPair2){
-        winMessage.textContent = "Two Pair"
-        win = 2
-      } else if (twoPair1 && jacksorBetter){
-        winMessage.textContent = "Jacks or better"
-        win = 1
-      } else {
-        winMessage.textContent = "No Winning Combinations"
-        win = 0
-      }
+  //main loop to run through the conditions and see what is true and not
+  if (valuesinOrder && fiveSuits && Math.min(...finalCardsValues) === 10) {
+    winMessage.textContent = "Royal Flush";
+    win = 250;
+  } else if (valuesinOrder && fiveSuits) {
+    winMessage.textContent = "Straight Flush";
+    win = 50;
+  } else if (fourPair) {
+    winMessage.textContent = "Four Pair";
+    win = 25;
+  } else if (threePair && twoPair1) {
+    winMessage.textContent = "Full House";
+    win = 9;
+  } else if (fiveSuits) {
+    winMessage.textContent = "Flush";
+    win = 6;
+  } else if (valuesinOrder) {
+    winMessage.textContent = "Straight";
+    win = 4;
+  } else if (threePair) {
+    winMessage.textContent = "Three Pair";
+    win = 3;
+  } else if (twoPair1 && twoPair2) {
+    winMessage.textContent = "Two Pair";
+    win = 2;
+  } else if (twoPair1 && jacksorBetter) {
+    winMessage.textContent = "Jacks or better";
+    win = 1;
+  } else {
+    winMessage.textContent = "No Winning Combinations";
+    win = 0;
+  }
 }
